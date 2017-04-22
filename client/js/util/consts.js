10582f50c04a62a4beb7e42388107be3be18fe5b
lambert <dyzxlingyang@163.com>
2017-04-23 00:55:41 +0800

    全屏左侧栏优化 && add consts.js

---
+++ client/js/util/consts.js
@@ -0,0 +1,14 @@
+module.exports = {
+	choiceArr : [{
+	    name:'地铁流量',
+	    open:true,
+	    items:[{title:'地铁',active:true},{title:'关键区域'}]
+	},{
+	    name:'公交流量',
+	    items:[{title:'地铁'},{title:'关键区域'}]
+	},{
+	    name:'OD',
+	    items:[{title:'地铁'},{title:'关键区域'}]
+	}]
+
+}
