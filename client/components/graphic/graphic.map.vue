241a50fc11a7236bcf6dcdbd971f513e5545dc81
hzliulingyang <hzliulingyang@corp.netease.com>
2017-06-26 23:59:07 +0800

    add graphic map

---
+++ client/components/graphic/graphic.map.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="m-frame m-graphic-map">
+        <baidu-map class="map" :center="{lng: 120.2, lat: 30.3}" :zoom="12" enable-traffic='true' ak="Wog9RN3H6TVTPs1IVT6FfKNEEuixUXQL">
+            <bm-traffic></bm-traffic>
+        </baidu-map>
+    </div>
+</template>
+
+<script>
+    import {BaiduMap} from 'vue-baidu-map';
+    import {BmTraffic} from 'vue-baidu-map';
+
+    export default {
+        name:'graphic-map',
+        data(){
+            return {}
+        },
+        props: ['queryResult','compontent'],
+        created(){
+            
+        },
+        mounted(){
+
+        },
+        methods: {
+            
+        },
+        components: {BaiduMap,BmTraffic}
+    }
+
+</script>
