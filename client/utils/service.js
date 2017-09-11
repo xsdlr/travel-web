65a1fc49e5a2cf27d380ecc6238a7c34e0eb57e1
xsdlr <xsdlr123@gmail.com>
2017-09-11 17:17:35 +0800

    remove api auth and id mapper value

--- client/utils/service.js
+++ client/utils/service.js
@@ -1,9 +1,8 @@
 import fetch from './fetch';
 import mockJson from './json';
-import md5 from 'blueimp-md5';
 
 // const hostIp = 'http://116.62.148.154:8080/odhbase';
-const hostIp = 'http://192.168.23.95:6009/';
+const hostIp = 'http://192.168.1.115/screenInterface/';
 const mock = false;
 
 export default {
@@ -15,7 +14,19 @@ export default {
 			});
 		}
 
-		let urlMap = {
+		const urlMap = {
+		  1: 'FeedFlow.php',
+      2: 'HospitalCount.php',
+      3: 'HospitalizeTime.php',
+      4: 'TripMode.php',
+      5: 'HospitalRank.php',
+      6: 'AgeContrast.php',
+      7: 'LeftGraph.php',
+      8: 'functionByAge.php',
+      9: 'FunctionByWeb.php',
+      10: 'FUnctionByMobile.php',
+      11: 'TerminalCount.php',
+      12: 'FunctionByTime.php',
 		  13: 'Functions.php',
 		  14: 'TotalFunction.php',
       15: 'Top3.php',
@@ -23,18 +34,23 @@ export default {
       17: 'ThirdPart.php',
       19: 'UserCount.php',
       20: 'UsageCount.php',
-      21: 'Retention.php'
+      21: 'Retention.php',
+      22: 'FunctionRank.php',
+      23: 'WalletCount.php',
+      26: 'PublicTransportComfort.php',
+      27: 'Transportation.php'
     };
 
-		return fetch(hostIp + urlMap[data.id],{
-			method:'POST',
-      data:{
-			  signature:md5('Change after installation' + '1.0' + JSON.stringify({}) +  '2017'),
-        ver:'1.0',
-        ts:'2017',
-        reqData:{}
-      }
-		});
+		return new Promise((resolve, reject) => {
+      const {id} = data || {};
+      const url = urlMap[id];
+      url ? resolve(url) : reject(`id为${id}的服务请求地址未映射`);
+    }).then(url => {
+      return fetch(hostIp + url,{
+        method:'POST',
+        data:{}
+      });
+    });
 	}
 
 }
