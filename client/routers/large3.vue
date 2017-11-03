c5ba3c4a2f0a29d65cbb8b052f1555b2c1810a3f
xsdlr <869412057@qq.com>
2017-11-03 16:02:39 +0800

    添加参数控制刷新开关、刷新时间

--- client/routers/large3.vue
+++ client/routers/large3.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="g-mn m-scrollbar" ref="container">
         <div class='m-dashRegion'>
-            <dashboardCanvas :config="config" v-bind:key="index"></dashboardCanvas>
+            <dashboardCanvas :config="config" :key="index"></dashboardCanvas>
         </div>
     </div>
 </template>
@@ -18,26 +18,52 @@
                     consts.LARGE3_CONFIG,
                     consts.LARGE4_CONFIG
                 ],
-                index: 0
+                index: 0,
+                interval: undefined
             }
         },
         computed: {
           config () {
             return this.configs[this.index]
+          },
+          query () {
+            return this.$route.query
+          },
+          needSwitch () {
+            return this.query.switch === '1'
+          },
+          switchTimeout () {
+            return Number(this.query.time) || 20000
           }
         },
         created(){
-            
+
         },
         mounted(){
-          setInterval(() => {
-            this.$nextTick(() => {
-              this.index = this.index < this.configs.length - 1 ? this.index + 1 : 0
-            })
-          }, 20000)
+          this.needSwitch && this.switchViews()
+        },
+        beforeDestroy () {
+            if (this.interval) {
+                clearInterval(this.interval);
+                this.interval = undefined;
+            }
         },
         methods: {
-            
+            switchViews () {
+              this.interval = setInterval(() => {
+                this.$nextTick(() => {
+                  this.index = this.index < this.configs.length - 1 ? this.index + 1 : 0
+                })
+              }, this.switchTimeout)
+            },
+            reload () {
+              this.$router.go(0)
+            }
+        },
+        watch: {
+          query () {
+              this.reload()
+          }
         },
         components:{dashboardCanvas}
     }
